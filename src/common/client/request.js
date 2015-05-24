305078591ed6bac57d3af63c72e2cc8a2f648d6a
Antoine Niek <antoine.niek@adgear.com>
2015-05-24 13:11:49 -0400

    fixes patch tryit

--- src/common/client/request.js
+++ src/common/client/request.js
@@ -105,7 +105,7 @@
 
   RAML.Client.Request = {
     create: function(url, method) {
-      return new RequestDsl({ url: url, method: method });
+      return new RequestDsl({ url: url, method: method.toUpperCase() });
     }
   };
 })();
